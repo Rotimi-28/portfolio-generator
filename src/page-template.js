9146593365c2143f6693b0557d0907e3052b0a27
Rotimi.E <matanmearoja@yahoo.com>
2022-03-24 16:58:39 -0700

    make correction on changes made

--- src/page-template.js
+++ src/page-template.js
@@ -1,26 +1,19 @@
-const generatePage = (Rotimi, Rotimihub) => {
+module.exports = (name, github) => {
     return `
-   <!DOCTYPE html> 
-    <html lang="en"> 
+    <!DOCTYPE html>
+    <html lang="en">
+    
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Portfolio Demo</title>
     </head>
-  
+    
     <body>
-     <h1>${Rotimi}</h1>
-      <h2><a href="https;//github.com/"${Rotimihub}>Github</a></h2>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
     </body>
     </html>
-     ;`
+    `;
   };
-  
-  fs.writeFile('index.html', generatePage(Rotimi, Rotimihub), err => {
-    if (err) throw new error(err);
-    
-    console.log('portifilio complete! check out index.html to see the output')
-  });
-
-module.exports = generatePage;
