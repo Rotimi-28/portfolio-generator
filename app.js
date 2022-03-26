2879e9e65eff85da846103257178422c7bebf745
Rotimi.E <matanmearoja@yahoo.com>
2022-03-26 14:09:03 -0700

    update progress

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [Rotimi, Rotimihub] = profileDataArgs;
-
+const inquirer = require("inquirer");
 /*const printProfileData = profileDataArr => {
   // This...
   for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -26,27 +19,27 @@ printProfileData(profileDataArgs);*/
 //console.log(generatePage('Jane', 'Janehub'));
 
 
-const generatePage = (Rotimi, Rotimihub) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
 
-  <body>
-   <h1>${Rotimi}</h1>
-    <h2><a href="https;//github.com/"${Rotimihub}>Github</a></h2>
-  </body>
-  </html>
-   ;`
-};
+//const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(Rotimi, Rotimihub), err => {
-  if (err) throw new error(err);
-  
-  console.log('portifilio complete! check out index.html to see the output')
-});
+//fs.writeFile('./index.html', pageHTML, err => {
+  //if (err) throw err;
+
+  //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+  inquirer
+  .prompt([
+    {
